0ed8710214eee59e8844a1a4d271ac7f68fc7bff
CampbellDavid <dcampbell092@gmail.com>
2020-05-07 10:51:12 +0100

    develop contact and my account pages

--- frontend/src/components/user/Contacts.js
+++ frontend/src/components/user/Contacts.js
@@ -1,1 +1,34 @@
-// Set-up as component for use in mainpage
+import React from 'react'
+import axios from 'axios'
+import Auth from '../../lib/auth'
+import { headers } from './../../lib/headers'
+
+class Contacts extends React.Component {
+	state = {
+		user: null,
+	}
+
+	async componentDidMount() {
+		const userId = Auth.getPayload().sub
+		try {
+			const res = await axios.get(`/api/users/${userId}`)
+			console.log(res.data)
+			this.setState({ user: res.data })
+			console.log(this.state.data)
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	render() {
+		console.log(this.state.user)
+		if (!this.state.user) return null
+		return (
+			<section className='mt-5 bg-color font'>
+				<h1 className='pt-5 text-center text-light'>Contacts</h1>
+			</section>
+		)
+	}
+}
+
+export default Contacts
--- frontend/src/components/user/Profile.js
+++ frontend/src/components/user/Profile.js
@@ -12,7 +12,7 @@ class Profile extends React.Component {
 		const userId = Auth.getPayload().sub
 		console.log('userId =', userId)
 		try {
-			const res = await axios.get(`/api/user/${userId}`)
+			const res = await axios.get(`/api/users/${userId}`)
 			console.log(res.data)
 			this.setState({ user: res.data })
 			console.log(this.state.data)
@@ -25,16 +25,11 @@ class Profile extends React.Component {
 		console.log(this.state.user)
 		if (!this.state.user) return null
 		return (
-			<section>
-				<div className='title-div'>
-					<h1>Profile</h1>
-				</div>
-				{/* <div className='profile-div'></div> */}
+			<section className='mt-5 bg-color font'>
+				<h1 className='pt-5 text-center text-light'>My Account</h1>
 			</section>
 		)
 	}
 }
 
 export default Profile
-
-// condense all pages into single page with multiple elements
