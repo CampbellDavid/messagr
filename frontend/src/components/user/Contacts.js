12e01485c4319fe99fef15ef7187f5931e7277ea
CampbellDavid <dcampbell092@gmail.com>
2020-05-09 11:10:44 +0100

    develop contact component

--- frontend/src/components/user/Contacts.js
+++ frontend/src/components/user/Contacts.js
@@ -26,6 +26,17 @@ class Contacts extends React.Component {
 		return (
 			<section className='mt-5 bg-color font'>
 				<h1 className='pt-5 text-center text-light'>Contacts</h1>
+				<div>
+					{this.state.user.contacts.length === 0 ? (
+						<h2 className='font'>No contacts</h2>
+					) : (
+						this.state.user.contacts.map((contact, i) => (
+							<h2 key={i} className='font'>
+								{contact.first_name}
+							</h2>
+						))
+					)}
+				</div>
 			</section>
 		)
 	}
